aa67dd95739810ba3ba82759c4021ad468f8ada6
Oleg Fedorov <olegajaro94@gmail.com>
2022-02-23 18:42:44 +0300

    Add QueueArray example

--- Queue/Queue_Example.playground/Contents.swift
+++ Queue/Queue_Example.playground/Contents.swift
@@ -1,3 +1,79 @@
 import UIKit
 
-var greeting = "Hello, playground"
+public protocol Queue {
+    associatedtype Element
+    
+    // вставить элемент в конец очереди
+    mutating func enqueue(_ element: Element) -> Bool
+    
+    // удалить элемент в начале очереди и вернуть его
+    mutating func dequeue() -> Element?
+    
+    // проверка пуста ли очередь
+    var isEmpty: Bool { get }
+    // возвращает элемент в начале очереди (не удаляя его)
+    var peek: Element? { get }
+}
+
+// MARK: - Использование массива для создания очереди
+public struct QueueArray<T>: Queue {
+    // связанный тип Element определяется параметром типа Т
+    public typealias Element = T
+    
+    private var array: [T] = []
+    
+    // При помощи возможностей массива получаем следующее
+    
+    // 1
+    // Проверка пуста ли очередь
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    // 2
+    // Возвращает элемент в начале очереди
+    public var peek: T? {
+        return array.first
+    }
+    
+    public init() { }
+    
+    // добавление в конец очереди
+    public mutating func enqueue(_ element: T) -> Bool {
+        array.append(element)
+        return true
+    }
+    
+    // если очередь пуста возвращает nil
+    // иначе удаляет первый элемент из очереди и возвращает его
+    public mutating func dequeue() -> T? {
+        return isEmpty ? nil : array.removeFirst()
+    }
+}
+
+extension QueueArray: CustomStringConvertible {
+    public var description: String {
+        return array.description
+    }
+}
+
+// MARK: - Test QueueArray
+var queue = QueueArray<String>()
+queue.enqueue("Ray")
+queue.enqueue("Brian")
+queue.enqueue("Eric")
+queue.dequeue()
+queue
+queue.peek
+
+/*
+                  Array-Based Queue
+ 
+ Operations       |   Best case    |  Worst case
+ -----------------|----------------|-------------
+ enqueue(_:)      |      O(1)      |     O(1)
+ ------------------------------------------------
+ dequeue(_:)      |      O(n)      |     O(n)
+ ------------------------------------------------
+ Space Complexity |      O(n)      |     O(n)
+ */
+
